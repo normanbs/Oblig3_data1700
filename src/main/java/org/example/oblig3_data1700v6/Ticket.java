c58225c100c85e2b3dc9329f5912df48bc8f1abf
Norman <norman.bonilla.sitaula@gmail.com>
2024-05-01 08:20:30 +0200

    Added ControllerKunde.java, Ticket.java, TicketRepo.java

---
+++ src/main/java/org/example/oblig3_data1700v6/Ticket.java
@@ -0,0 +1,97 @@
+package org.example.oblig3_data1700v6;
+
+/*
+@Entity
+@Table(name = "ticket")*/
+public class Ticket {
+ /*   @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ticket_sequence")
+    @SequenceGenerator(name = "ticket_sequence", sequenceName = "ticket_sequence", allocationSize = 1)
+  */
+    private int ID;
+
+    private String film;
+    private int antall;
+    private String fornavn;
+    private String etternavn;
+    private String telefon;
+    private String epost;
+
+    public Ticket() {}
+    public Ticket(int ID, String film, int antall, String fornavn, String etternavn, String telefon, String epost) {
+        this.ID = ID;
+        this.film = film;
+        this.antall = antall;
+        this.fornavn = fornavn;
+        this.etternavn = etternavn;
+        this.telefon = telefon;
+        this.epost = epost;
+    }
+    public String getFilm() {
+        return film;
+    }
+
+    public void setFilm(String film) {
+        this.film = film;
+    }
+
+    public int getAntall() {
+        return antall;
+    }
+
+    public void setAntall(int antall) {
+        this.antall = antall;
+    }
+
+    public String getFornavn() {
+        return fornavn;
+    }
+
+    public void setFornavn(String fornavn) {
+        this.fornavn = fornavn;
+    }
+
+    public String getEtternavn() {
+        return etternavn;
+    }
+
+    public void setEtternavn(String etternavn) {
+        this.etternavn = etternavn;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+
+    public String getEpost() {
+        return epost;
+    }
+
+    public void setEpost(String epost) {
+        this.epost = epost;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+    @Override
+    public String toString() {
+        return "Ticket{" +
+                "ID=" + ID +
+                ", film='" + film + '\'' +
+                ", antall=" + antall +
+                ", fornavn='" + fornavn + '\'' +
+                ", etternavn='" + etternavn + '\'' +
+                ", telefon='" + telefon + '\'' +
+                ", epost='" + epost + '\'' +
+                '}';
+    }
+}
